ddd529e03912edef03915bb1b508b5f2e405b684
tkorays <tkorays@hotmail.com>
2015-05-11 10:30:32 +0800

    rebuild

--- genfile.sh
+++
@@ -1,2 +0,0 @@
-cat ./template.markdown > ./_posts/$(date "+%Y-%m-%d")-$1.markdown
-echo ./_posts/$(date "+%Y-%m-%d")-$1.markdown
--- static/js/localhost.js
+++
@@ -1,9 +0,0 @@
-require.config({
-	baseUrl: "/static/js/libs/",
-	paths: {
-		"jquery": "jquery.min",
-	}
-});
-require(['jquery','utils'],function($,utils){
-	
-});
